2a9ee7dbff2b44598ba3a33b542455fb6bf3d86b
Jaesang Lee <jaesang_lee@sk.com>
2022-03-15 06:33:43 +0000

    Add cmd for deleting cluster

---
+++ cmd/cluster_delete.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2022 SK Telecom <https://github.com/openinfradev>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/openinfradev/tks-proto/tks_pb"
+	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+// clusterDeleteCmd represents the delete command
+var clusterDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a TACO Cluster.",
+	Long: `Delete a TACO Cluster to AWS.
+	
+Example:
+tks cluster delete <CLUSTER_ID>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("You must specify cluster name.")
+			fmt.Println("Usage: tks cluster delete <CLUSTER_ID>")
+			os.Exit(1)
+		}
+		var conn *grpc.ClientConn
+		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %s", err)
+		}
+		defer conn.Close()
+
+		client := pb.NewClusterLcmServiceClient(conn)
+		ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Minute)
+		defer cancel()
+        data := make([]pb.IDRequest, 1)
+        data[0].Id = args[0]
+		m := protojson.MarshalOptions{
+			Indent:        "  ",
+			UseProtoNames: true,
+		}
+		jsonBytes, _ := m.Marshal(&data[0])
+		fmt.Println("Proto Json data...")
+		fmt.Println(string(jsonBytes))
+		r, err := client.DeleteCluster(ctx, &data[0])
+		fmt.Println(r)
+        if err != nil {
+            fmt.Println(err)
+        } else {
+            fmt.Println("The request to delete cluster ", args[0], " was accepted.")
+        }
+	},
+}
+
+func init() {
+	clusterCmd.AddCommand(clusterDeleteCmd)
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/matryer/is v1.4.0
 	github.com/openinfradev/tks-contract v0.1.1-0.20210928021110-fe2b666327cc
-	github.com/openinfradev/tks-proto v0.0.6-0.20211015003551-ed8f9541f40d
+	github.com/openinfradev/tks-proto v0.0.6-0.20220304011855-99ac351f420f
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.9.0
 	github.com/stretchr/testify v1.7.0
